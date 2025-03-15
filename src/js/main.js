548a78eecd36dce1d569e9932732c00251f718d4
daniel3034 <danielh3034@gmail.com>
2025-03-15 13:54:00 -0500

    Fixed search bar and updated ProductList

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,3 @@ import ProductList from "./ProductList.mjs";
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
-
-listing.init();
